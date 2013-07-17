4015d76e9419d6100e09a8cba37c622350bd8cca
root <root@tsask-form.(none)>
2013-07-17 16:47:58 +0000

    fixed login function

--- web/js/login.js
+++ web/js/login.js
@@ -1,29 +1,31 @@
 (function(){
 			var username = document.getElementById('signin_username'),
 			    //passwordText = document.getElementById('passwordText'),
-				password = document.getElementById('signin_password');
-			username.onfocus = function(){
-				if (username.value == 'Username') {
-					username.value = '';
-					username.style.color = '#000';
-				}
-			};
-			username.onblur = function(){
-				if (username.value == ''){
-					username.value = 'Username';
-					username.style.color = '#999';
-				}
-			};
-			password.onfocus = function(){
-				if (password.value == '******') {
-					password.value = '';
-					password.style.color = '#000';
-				}			
-			};
-			password.onblur = function(){
-				if (password.value == ''){
-					password.value = '******';
-					password.style.color = '#999';
-				}
-			};
+			password = document.getElementById('signin_password');
+			if (username && password) {
+				username.onfocus = function(){
+					if (username.value == 'Username') {
+						username.value = '';
+						username.style.color = '#000';
+					}
+				};
+				username.onblur = function(){
+					if (username.value == ''){
+						username.value = 'Username';
+						username.style.color = '#999';
+					}
+				};
+				password.onfocus = function(){
+					if (password.value == '******') {
+						password.value = '';
+						password.style.color = '#000';
+					}			
+				};
+				password.onblur = function(){
+					if (password.value == ''){
+						password.value = '******';
+						password.style.color = '#999';
+					}
+				};
+			}
 		})();
