72a82742e84aedd94b63752ebfa40f40372233c8
root <root@tsask-form.(none)>
2013-09-03 23:05:36 +0000

    sql insertation works

--- csv_exporter/src/csvexporter/core.clj
+++ csv_exporter/src/csvexporter/core.clj
@@ -1,15 +1,28 @@
 (ns csvexporter.core
     (:gen-class)
     (:use compojure.core 
+          csvexporter.env
           ring.adapter.jetty)
     (:require [compojure.handler :as handler]
               [ring.middleware.session :as session]
               [ring.middleware.params :as params]
               [ring.middleware.multipart-params :as mulparams]
+              [clojure.java.jdbc :as j]
+              [clojure.java.jdbc.sql :as sql]
               [compojure.route :as route]))
 
 (defn csv [params]
-  (println params))
+  (j/insert! SQLDB :CSV_report 
+             {:app_name (:app_name params) 
+              :address (:address params) 
+              :phone (:phone params) 
+              :email (:email params) 
+              :app_type (:app_type params) 
+              :app_detail (:app_detail params) 
+              :invoice_id (:invoice_id params) 
+              :paid_by (:paid_by params) 
+              :card_type (:card_type params) 
+              :payment_amt (:payment_amt params)}))
 
 (defroutes app-routes
   (route/resources "/") 
--- csv_exporter/src/csvexporter/env.clj
+++ csv_exporter/src/csvexporter/env.clj
@@ -1,6 +1,6 @@
 (ns csvexporter.env)
 
-(def mysql-db {:subprotocol "mysql"
+(def SQLDB {:subprotocol "mysql"
                               :subname "//127.0.0.1:3306/authority"
                               :user "root"
                               :password "121212"})
