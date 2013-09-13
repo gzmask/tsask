80819b008f800e8c71e1bd68143d02b00621d5ed
root <root@tsask-form.(none)>
2013-09-13 16:33:21 +0000

    update php side nav

--- apps/backend/templates/_header.php
+++ apps/backend/templates/_header.php
@@ -1,5 +1,4 @@
   <div class="header">
     <div class="icon_logo"><a href="#" title="technical safety authority interface design"><img src="/images/icon_logo.jpg" alt="technical safety authority interface design"/></a></div>
-    <a href="http://www.tsaskforms.ca:3000/csvs/export" title="CSV exporter">export CSV</a>
     <div class="icon_exit"><a href="<?php echo url_for('@sf_guard_signout');?>" title="logOut">log Out</a></div>
   </div>
